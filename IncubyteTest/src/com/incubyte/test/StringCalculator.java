c106097478cf9c345e9ca158719315ead4f39fe4
Ajay Ingale <ajayingale44@gmail.com>
2021-10-17 12:45:01 +0530

    singlevalue_string_returns_1_Test_SuccessFul

--- IncubyteTest/src/com/incubyte/test/StringCalculator.java
+++ IncubyteTest/src/com/incubyte/test/StringCalculator.java
@@ -3,7 +3,13 @@ package com.incubyte.test;
 public class StringCalculator {
 
 	public int calculate(String input) {
-		return 0;
-	}
+          if(input.isEmpty())
+          {
+        	  return 0;
+          }
+          else {
+        	  return Integer.parseInt(input);
+          }
+}
 	
 }
