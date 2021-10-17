9a964988021a68bd8807041d1bea7e417f4f29fe
Ajay Ingale <ajayingale44@gmail.com>
2021-10-17 22:36:15 +0530

    Add_Negative_number_multiple_negative_tests_Passed

--- IncubyteTest/src/com/incubyte/test/StringCalculator.java
+++ IncubyteTest/src/com/incubyte/test/StringCalculator.java
@@ -1,8 +1,12 @@
 package com.incubyte.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
+	
 	private String delimiter="\n|,|;";
-	public int add(String input) {
+	public int add(String input) throws Exception {
 		String [] numbers =input.split(delimiter);
           if(isEmpty(input))
           {
@@ -25,15 +29,42 @@ public class StringCalculator {
         	    }
         	    return getSum(newnumbers); 
           }
+         
+           
           
           else {
         	  return getSum(numbers);
           }
+          
+          
 	
 		
 	}
 	
-	private int getSum(String[] numbers) {
+	private int getSum(String[] numbers) throws Exception {
+		 int count = 0;
+		for(String current :numbers) {
+			if(StringToInt(current)<0) {
+				 System.out.print(current+" ");
+				  count++;
+				 continue;	
+				
+					 
+				   
+			 }
+			
+			 
+			
+		}
+		 if ( count ==1)
+		 {
+			 throw new Exception("Negative Not Allowed");
+		 }
+		 if(count>1)
+		 {
+			 throw new Exception("Negatives Not Allowed");
+		 } 
+		 
 		int sum=0;
 		for(String current :numbers) {
 		
@@ -55,6 +86,6 @@ public class StringCalculator {
 	
 	
 	
-	
     
 }
+
--- IncubyteTest/src/com/incubyte/test/StringCalculatorTest.java
+++ IncubyteTest/src/com/incubyte/test/StringCalculatorTest.java
@@ -6,34 +6,59 @@ import org.junit.jupiter.api.Test;
 
 class StringCalculatorTest {
 	@Test
-	public void empty_string_should_return_0()  {
+	public void empty_string_should_return_0() throws Exception  {
 		StringCalculator calculator =new StringCalculator();
 		assertEquals(calculator.add(""), 0);
 	}
 	@Test
-	public void singlevalue_string_should_return_1()  {
+	public void singlevalue_string_should_return_1() throws Exception  {
 		StringCalculator calculator =new StringCalculator();
 		assertEquals(calculator.add("1"), 1);
 	}
 	@Test
-	public void twovalue_string_should_return_sum()  {
+	public void twovalue_string_should_return_sum() throws Exception  {
 		StringCalculator calculator =new StringCalculator();
 		assertEquals(calculator.add("1,2"),3);
 	}
 	@Test
-	public void threevalue_string_should_return_sum()  {
+	public void threevalue_string_should_return_sum() throws Exception  {
 		StringCalculator calculator =new StringCalculator();
 		assertEquals(calculator.add("1,2,3"),6);
 	}
 	@Test
-	public void  newlinebetween_string_should_return_sum()  {
+	public void  newlinebetween_string_should_return_sum() throws Exception  {
 		StringCalculator calculator =new StringCalculator();
 		assertEquals(calculator.add("1\n2,3"),6);
 	}
 	@Test
-	public  void whenDelimiterIsSpecifiedThenItIsUsedToSeparateNumbers() {
+	public  void whenDelimiterIsSpecifiedThenItIsUsedToSeparateNumbers() throws Exception {
 		StringCalculator calculator =new StringCalculator();
 	   assertEquals(calculator.add("//;\n1;2"),3);
 	}
+	@Test
+	@org.junit.Test(expected = RuntimeException.class)
+	public final void whenNegativeNumberIsUsedThenRuntimeExceptionIsThrown() {
+		StringCalculator calculator =new StringCalculator();
+		try {
+			calculator.add("-3");
+		
+		} catch (Exception e) {
+			 System.out.println(e.getMessage());
+		
+		}
+	}
+	@Test
+	@org.junit.Test(expected = RuntimeException.class)
+	public final void whenNegativeNumbersIsUsedThenRuntimeExceptionIsThrown() {
+		StringCalculator calculator =new StringCalculator();
+		try {
+			calculator.add("-3,-6,-15,-18,46,33");
+		
+		} catch (Exception e) {
+			 System.out.println(e.getMessage());
+		
+		}
+	}
+	
 	
 }
